361949aca0c93ec4f20eb5fa5e99d0fd29b6ee76
Cassio Pagnoncelli <cassiopagnoncelli@gmail.com>
2023-04-02 19:46:24 -0300

    Reorganize generate_gid

--- app/models/stern/application_record.rb
+++ app/models/stern/application_record.rb
@@ -6,5 +6,10 @@ module Stern
       super
       # subclass.table_name = ... # rename table
     end
+
+    # Generates an unique gid to be used across operations, txs, and entries.
+    def self.generate_gid
+      ActiveRecord::Base.connection.execute("SELECT nextval('gid_seq')").first.values.first
+    end
   end
 end
--- lib/stern.rb
+++ lib/stern.rb
@@ -2,15 +2,15 @@ require "stern/version"
 require "stern/engine"
 
 module Stern
+  def self.generate_gid
+    ApplicationRecord.generate_gid
+  end
+
   def self.outstanding_balance(book_id = :merchant_balance, timestamp = DateTime.current)
     OutstandingBalanceQuery.new(book_id:, timestamp:).call
   end
 
   def self.balance(gid, book_id = :merchant_balance, timestamp = DateTime.current)
     BalanceQuery.new(gid:, book_id:, timestamp:).call
-  end
-
-  def self.generate_gid
-    ActiveRecord::Base.connection.execute("SELECT nextval('gid_seq')").first.values.first
   end
 end
