e122672dd11412b847128c2271ea3622c64c3ff7
Cassio Pagnoncelli <cassiopagnoncelli@gmail.com>
2024-03-06 20:15:58 -0300

    Fix deprecation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -64,6 +64,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   #-- Custom code.
+  config.raise_errors_for_deprecations!
+
   # Shoulda matchers.
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
