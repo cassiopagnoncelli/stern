7b7775c97423f94251bfdfdb92326ce27016ab67
Cassio Pagnoncelli <cassiopagnoncelli@gmail.com>
2024-03-06 21:01:49 -0300

    Fix migration cops, fix migration if_not_exists

--- db/migrate/20230522052219_create_stern_operations.rb
+++ db/migrate/20230522052219_create_stern_operations.rb
@@ -1,6 +1,6 @@
 class CreateSternOperations < ActiveRecord::Migration[7.0]
   def change
-    create_table :stern_operations do |t|
+    create_table :stern_operations, if_not_exists: true do |t|
       t.integer :operation_def_id, null: false, index: true
       t.integer :direction, null: false
       t.json :params, null: false, default: "{}"
