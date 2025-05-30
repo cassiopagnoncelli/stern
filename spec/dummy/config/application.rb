b0e6bd74f837d6960d46e51acc4a625f011f7ded
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-05-30 06:23:39 -0300

    Update Rails engine to 8.0 (remainder)

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,9 +3,9 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
-# require "active_storage/engine"
+require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
@@ -17,14 +17,16 @@ require "action_view/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "stern"
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults Rails::VERSION::STRING.to_f
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 8.0
 
-    # For compatibility with applications that use this config
-    config.action_controller.include_all_helpers = false
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -36,10 +38,5 @@ module Dummy
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # Set timezone so DateTime/Time.current works properly
-    config.time_zone = "America/Sao_Paulo"
-
-    # config.active_record.schema_format = :sql
   end
 end
