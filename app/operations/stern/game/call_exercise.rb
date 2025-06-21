1d3a4f853064444d6e92b92c0b62f6a186f3a5bc
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-06-21 17:03:30 -0300

    Fix call/put exercises

--- app/operations/stern/game/call_exercise.rb
+++ app/operations/stern/game/call_exercise.rb
@@ -5,12 +5,13 @@ module Stern
   class CallExercise < BaseOperation
     include ActiveModel::Validations
 
-    attr_accessor :call_option_id, :customer_id, :currency, :amount
+    attr_accessor :call_option_id, :customer_id, :currency, :amount, :payout
 
     validates :call_option_id, presence: true, numericality: { other_than: 0 }
     validates :customer_id, presence: true, numericality: { other_than: 0 }
     validates :currency, presence: true, allow_blank: false, allow_nil: false
     validates :amount, presence: true
+    validates :payout, presence: true
 
     UnknownCurrencyError = Class.new(StandardError)
 
@@ -20,11 +21,13 @@ module Stern
     # @param customer_id [Bigint] customer id
     # @param currency [Str] curreny code (eg. usd, eur, btc)
     # @param amount [Bigint] amount placed on the call option
-    def initialize(call_option_id: nil, customer_id: nil, currency: nil, amount: nil)
+    # @param payout [Bigint] payout amount
+    def initialize(call_option_id: nil, customer_id: nil, currency: nil, amount: nil, payout: nil)
       self.call_option_id = call_option_id
       self.customer_id = customer_id
       self.currency = currency.strip.downcase.presence
       self.amount = amount
+      self.payout = payout
     end
 
     def perform(operation_id)
@@ -33,8 +36,8 @@ module Stern
       raise UnknownCurrencyError unless currency.presence&.in?(%w[usd])
 
       EntryPair.add_handle_payback_call_usd(call_option_id, customer_id, amount, nil, operation_id:) if amount.present?
-      EntryPair.add_payout_call_usd(call_option_id, customer_id, amount, nil, operation_id:) if amount.present?
-      EntryPair.add_bops_trade_pl_usd(call_option_id, customer_id, amount, nil, operation_id:) if amount.present?
+      EntryPair.add_payout_call_usd(call_option_id, customer_id, payout, nil, operation_id:) if payout.present?
+      EntryPair.add_bops_trade_pl_usd(call_option_id, customer_id, payout, nil, operation_id:) if payout.present?
     end
 
     def perform_undo
