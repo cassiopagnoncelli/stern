e79a356c6f0356848f772da82d1b51cc51acc211
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-04-21 11:11:53 -0300

    Enforce amount validation

--- app/models/stern/entry_pair.rb
+++ app/models/stern/entry_pair.rb
@@ -12,7 +12,11 @@ module Stern
 
     validates :code, presence: true
     validates :uid, presence: true, uniqueness: { scope: [:code] }
-    validates :amount, presence: true
+    validates :amount, presence: true, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: -9_223_372_036_854_775_808,
+      less_than_or_equal_to: 9_223_372_036_854_775_807,
+    }
     validate :no_future_timestamp, on: :create
 
     before_save do
