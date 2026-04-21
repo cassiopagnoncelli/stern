ef6a026506334a169d189569ccc8328d9f2501f3
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-04-21 15:15:23 -0300

    Simplify currencies

--- config/initializers/currencies.rb
+++ config/initializers/currencies.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 module Stern
-  _currencies_catalog = YAML.load_file(Engine.root.join("config/currencies_catalog.yaml"))
+  STERN_CURRENCIES ||= YAML.load_file(Engine.root.join("config/currencies_catalog.yaml")).freeze
 
-  STERN_CURRENCIES ||= _currencies_catalog.freeze
-
-  STERN_CURRENCIES_R ||= _currencies_catalog
+  STERN_CURRENCIES_R ||= STERN_CURRENCIES
     .each_with_object({}) { |(name, idx), h| h[idx] = name if h[idx].nil? || name == name.upcase }
     .freeze
 end
