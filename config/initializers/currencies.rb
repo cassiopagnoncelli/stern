393ebd67e2b6dc858be301e495714100658f7798
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-04-21 12:24:22 -0300

    Rename currencies catalog

--- config/initializers/currencies.rb
+++ config/initializers/currencies.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module Stern
-  _raw = YAML.load_file(Engine.root.join("config/currencies_catalog.yaml"))
+  _currencies_catalog = YAML.load_file(Engine.root.join("config/currencies_catalog.yaml"))
 
-  STERN_CURRENCIES ||= _raw.freeze
+  STERN_CURRENCIES ||= _currencies_catalog.freeze
 
-  STERN_CURRENCIES_R ||= _raw
+  STERN_CURRENCIES_R ||= _currencies_catalog
     .each_with_object({}) { |(name, idx), h| h[idx] = name if h[idx].nil? || name == name.upcase }
     .freeze
 end
