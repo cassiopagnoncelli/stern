6c33e12d422eafa2b85871fac313355315015210
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-05-03 19:45:59 -0300

    Simplify books

--- app/operations/stern/general/charge_pix.rb
+++ app/operations/stern/general/charge_pix.rb
@@ -13,11 +13,7 @@ module Stern
     validates :currency, presence: true, allow_blank: false, allow_nil: false
 
     def target_tuples
-      tuples = tuples_for_pair(:pay_pix, payment_id, currency)
-      tuples += tuples_for_pair(:pp_charge_pix, payment_id, currency)
-      tuples += tuples_for_pair(:pp_charge, payment_id, currency)
-      tuples += customer_id ? tuples_for_pair(:charge_identified_customer, customer_id, currency)
-                            : tuples_for_pair(:charge_unidentified_customer, 1, currency)
+      tuples = tuples_for_pair(:charge_pix_payment, charge_id, currency)
       tuples
     end
 
@@ -25,16 +21,7 @@ module Stern
       raise ArgumentError if invalid? || operation_id.blank?
 
       # Operational info pairs.
-      EntryPair.add_pay_pix(charge_id, payment_id, amount, currency, operation_id:)
-
-      # Accounting info pairs.
-      EntryPair.add_pp_charge_pix(charge_id, payment_id, amount, currency, operation_id:)
-      EntryPair.add_pp_charge(charge_id, payment_id, amount, currency, operation_id:)
-      if customer_id
-        EntryPair.add_charge_identified_customer(charge_id, customer_id, amount, currency, operation_id:)
-      else
-        EntryPair.add_charge_unidentified_customer(charge_id, 1, amount, currency, operation_id:)
-      end
+      EntryPair.add_charge_pix_payment(charge_id, payment_id, amount, currency, operation_id:)
     end
   end
 end
