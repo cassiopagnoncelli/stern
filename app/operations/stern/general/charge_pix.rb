c16a2e5646717250bf1e18496beaa4fc3175bb8a
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-05-04 03:09:23 -0300

    Draft new charge pix

--- app/operations/stern/general/charge_pix.rb
+++ app/operations/stern/general/charge_pix.rb
@@ -4,23 +4,39 @@ module Stern
   class ChargePix < BaseOperation
     include ActiveModel::Validations
 
-    inputs :charge_id, :payment_id, :customer_id, :amount, :currency, :fee
+    inputs :charge_id, :payment_id, :merchant_id, :customer_id, :amount, :currency, :fee
 
     validates :charge_id, presence: true, numericality: { greater_than: 0, only_integer: true }
     validates :payment_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+    validates :merchant_id, presence: true, numericality: { greater_than: 0, only_integer: true }
     validates :customer_id, numericality: { greater_than: 0, only_integer: true, allow_nil: true }
     validates :amount, presence: true, numericality: { other_than: 0, only_integer: true }
     validates :currency, presence: true, allow_blank: false, allow_nil: false
 
     def target_tuples
-      tuples_for_pair(:charge_pix_payment, charge_id, payment_id, currency)
+      tuples = tuples_for_pair(:charge_with_pix, charge_id, charge_id, currency)
+      tuples += tuples_for_pair(:payment_with_pix, payment_id, payment_id, currency)
+      tuples += tuples_for_pair(:merchant_payment, merchant_id, merchant_id, currency)
+      tuples += customer_id.present? ?
+        tuples_for_pair(:identified_customer_payment, customer_id, customer_id, currency) :
+        tuples_for_pair(:unidentified_customer_payment, 1, 1, currency)
+      tuples
     end
 
     def perform(operation_id)
       raise ArgumentError if invalid? || operation_id.blank?
 
       # Operational info pairs.
-      EntryPair.add_charge_pix_payment(charge_id, payment_id, amount, currency, operation_id:)
+      EntryPair.add_payment_with_pix(payment_id, payment_id, amount, currency, operation_id:)
+
+      # Accounting
+      EntryPair.add_charge_with_pix(charge_id, charge_id, amount, currency, operation_id:)
+      EntryPair.add_merchant_payment(merchant_id, merchant_id, amount, currency, operation_id:)
+      if customer_id.present?
+        EntryPair.add_identified_customer_payment(customer_id, customer_id, amount, currency, operation_id:)
+      else
+        EntryPair.add_unidentified_customer_payment(customer_id, 1, amount, currency, operation_id:)
+      end
     end
   end
 end
---
+++ app/operations/stern/general/split_payment.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Stern
+  class SplitPayment < BaseOperation
+    include ActiveModel::Validations
+
+    inputs :charge_id, :payment_id, :customer_id, :amount, :currency, :fee
+
+    validates :charge_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+    validates :payment_id, presence: true, numericality: { greater_than: 0, only_integer: true }
+    validates :customer_id, numericality: { greater_than: 0, only_integer: true, allow_nil: true }
+    validates :amount, presence: true, numericality: { other_than: 0, only_integer: true }
+    validates :currency, presence: true, allow_blank: false, allow_nil: false
+
+    def target_tuples
+      tuples_for_pair(:charge_pix_payment, charge_id, payment_id, currency)
+    end
+
+    def perform(operation_id)
+      raise ArgumentError if invalid? || operation_id.blank?
+
+      # Operational info pairs.
+      EntryPair.add_split(charge_id, payment_id, amount, currency, operation_id:)
+    end
+  end
+end
