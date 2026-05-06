c62a35c3288860a1db8f72094bab42371980c8d4
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-05-06 03:13:08 -0300

    Charge payment

--- app/operations/stern/general/charge_payment.rb
+++ app/operations/stern/general/charge_payment.rb
@@ -1,26 +1,27 @@
 # frozen_string_literal: true
 
 module Stern
-  class ChargePix < BaseOperation
+  class ChargePayment < BaseOperation
     include ActiveModel::Validations
 
-    inputs :charge_id, :payment_id, :merchant_id, :customer_id, :amount, :currency
+    PAYMENT_METHODS = %w[bank_transfer credit_card wallet pix].freeze
+
+    inputs :charge_id, :payment_id, :method, :amount, :currency
 
     validates :charge_id, presence: true, numericality: { greater_than: 0, only_integer: true }
     validates :payment_id, presence: true, numericality: { greater_than: 0, only_integer: true }
-    validates :merchant_id, presence: true, numericality: { greater_than: 0, only_integer: true }
-    validates :customer_id, numericality: { greater_than: 0, only_integer: true, allow_nil: true }
+    validates :method, presence: true, inclusion: { in: PAYMENT_METHODS }
     validates :amount, presence: true, numericality: { other_than: 0, only_integer: true }
     validates :currency, presence: true, allow_blank: false, allow_nil: false
 
     def target_tuples
-      tuples_for_pair(:charge_pix, charge_id, payment_id, currency)
+      tuples_for_pair("charge_#{method}".to_sym, charge_id, payment_id, currency)
     end
 
     def perform(operation_id)
       raise ArgumentError if invalid? || operation_id.blank?
 
-      EntryPair.add_charge_pix(charge_id, payment_id, amount, currency, operation_id:)
+      EntryPair.public_send("add_charge_#{method}".to_sym, charge_id, payment_id, amount, currency, operation_id:)
     end
   end
 end
---
+++ app/operations/stern/general/charge_payment_fee.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Stern
+  class ChargePaymentFee < BaseOperation
+    include ActiveModel::Validations
+
+    PAYMENT_METHODS = %w[bank_transfer credit_card wallet pix].freeze
+
+    inputs :merchant_id, :customer_id, :partner_id, :amount, :currency
+
+    validates :merchant_id, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+    validates :customer_id, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+    validates :partner_id, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+    validates :method, presence: true, inclusion: { in: PAYMENT_METHODS }
+    validates :amount, presence: true, numericality: { other_than: 0, only_integer: true }
+    validates :currency, presence: true, allow_blank: false, allow_nil: false
+
+    def target_tuples
+      if merchant_id.present?
+        tuples_for_pair("charge_#{method}_fee_merchant".to_sym, merchant_id, merchant_id, currency)
+      elsif customer_id.present?
+        tuples_for_pair("charge_#{method}_fee_customer".to_sym, customer_id, customer_id, currency)
+      elsif partner_id.present?
+        tuples_for_pair("charge_#{method}_fee_partner".to_sym, partner_id, partner_id, currency)
+      end
+    end
+
+    def perform(operation_id)
+      raise ArgumentError if invalid? || operation_id.blank?
+      raise ArgumentError if [merchant_id, customer_id, partner_id].compact.count != 1
+
+      if merchant_id.present?
+        EntryPair.public_send("add_charge_#{method}_fee_merchant".to_sym, merchant_id, merchant_id, fee, currency, operation_id:)
+      elsif customer_id.present?
+        EntryPair.public_send("add_charge_#{method}_fee_customer".to_sym, customer_id, customer_id, amount, currency, operation_id:)
+      elsif partner_id.present?
+        EntryPair.public_send("add_charge_#{method}_fee_partner".to_sym, partner_id, partner_id, amount, currency, operation_id:)
+      end
+    end
+  end
+end
