2eb1e830f55bd5eea13fc1ef60fb8caea6e8a3ed
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-05-06 07:44:27 -0300

    Add deposit

---
+++ app/operations/stern/general/deposit.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Stern
+  class Deposit < BaseOperation
+    inputs :merchant_id, :customer_id, :partner_id, :amount, :currency
+
+    validates :merchant_id, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+    validates :customer_id, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+    validates :partner_id, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+    validates :amount, presence: true, numericality: { other_than: 0, only_integer: true }
+    validates :currency, presence: true, allow_blank: false, allow_nil: false
+    validates_exactly_one_of :merchant_id, :customer_id, :partner_id
+
+    def target_tuples
+      if merchant_id.present?
+        tuples_for_pair(:deposit_merchant, merchant_id, merchant_id, currency)
+      elsif customer_id.present?
+        tuples_for_pair(:deposit_customer, customer_id, customer_id, currency)
+      elsif partner_id.present?
+        tuples_for_pair(:deposit_partner, partner_id, partner_id, currency)
+      else
+        []
+      end
+    end
+
+    def perform(operation_id)
+      if merchant_id.present?
+        EntryPair.add_deposit_merchant(merchant_id, merchant_id, amount, currency, operation_id:)
+      elsif customer_id.present?
+        EntryPair.add_deposit_customer(customer_id, customer_id, amount, currency, operation_id:)
+      elsif partner_id.present?
+        EntryPair.add_deposit_partner(partner_id, partner_id, amount, currency, operation_id:)
+      end
+    end
+  end
+end
