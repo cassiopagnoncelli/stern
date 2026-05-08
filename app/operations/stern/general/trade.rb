e2ab2466107e83c9f865219bef85206cf5e15159
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2026-05-08 02:18:30 -0300

    Fix trade

--- app/operations/stern/general/trade.rb
+++ app/operations/stern/general/trade.rb
@@ -2,24 +2,24 @@
 
 module Stern
   class Trade < BaseOperation
-    inputs :trade_id, :customer_id, :amount, :fee, :currency
+    inputs :trade_id, :provision_id, :amount, :fee, :currency
 
     validates :trade_id, numericality: { greater_than: 0, only_integer: true }
-    validates :customer_id, numericality: { greater_than: 0, only_integer: true }
+    validates :provision_id, numericality: { greater_than: 0, only_integer: true }
     validates :amount, presence: true, numericality: { only_integer: true }
     validates :fee, presence: true, numericality: { only_integer: true }
     validates :currency, presence: true, allow_blank: false, allow_nil: false
 
     def target_tuples
       tuples = []
-      tuples += tuples_for_pair(:allocation_trade, trade_id, customer_id, currency) unless amount.zero?
-      tuples += tuples_for_pair(:allocation_trade_fee, trade_id, customer_id, currency) unless fee.zero?
+      tuples += tuples_for_pair(:provision_trade, trade_id, provision_id, currency) unless amount.zero?
+      tuples += tuples_for_pair(:provision_trade_fee, trade_id, provision_id, currency) unless fee.zero?
       tuples
     end
 
     def perform(operation_id)
-      EntryPair.add_allocation_trade(trade_id, customer_id, amount, currency, operation_id:) unless amount.zero?
-      EntryPair.add_allocation_trade_fee(trade_id, customer_id, -fee, currency, operation_id:) unless fee.zero?
+      EntryPair.add_provision_trade(trade_id, provision_id, amount, currency, operation_id:) unless amount.zero?
+      EntryPair.add_provision_trade_fee(trade_id, provision_id, -fee, currency, operation_id:) unless fee.zero?
     end
   end
 end
